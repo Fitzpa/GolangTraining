a567f44d2f1cc68c14b0f6870c12fff4e7ae8efc
unknown <fitz4492@gmail.com>
2020-04-19 19:54:22 -0500

    completed 5 Goland exercises and worked through primitive data types and var naming conventions

---
+++ 28_go-programming-exercises/01/main.go
@@ -0,0 +1,23 @@
+// Hands-on exercise #1
+// Using the short declaration operator, ASSIGN these VALUES to VARIABLES with the IDENTIFIERS “x” and “y” and “z”
+// 42
+// “James Bond”
+// true
+// Now print the values stored in those variables using
+// a single print statement
+// multiple print statements
+package main
+
+import "fmt"
+
+func main() {
+	a := 42
+	b := "Bond, James"
+	c := true
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+
+	fmt.Println(a, b, c)
+}
